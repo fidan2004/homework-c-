ede2afad5214cc33ccc78a7744bd7ebf935e82f1
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-09-19 22:27:00 +0400

    Add files via upload

---
+++ http/ConsoleApp38/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+    internal class Program
+    {
+        static async  Task Main(string[] args)
+
+        {
+            #region task1
+            //const string Url = "https://jsonplaceholder.typicode.com/todos";
+            //HttpClient client = new HttpClient();
+
+
+            //    var result = await client.GetStringAsync(Url);
+            //Console.WriteLine(result);
+            #endregion
+
+            await GetToDo();
+        }
+
+        public static async Task GetToDo()
+        {
+            const string url = "https://jsonplaceholder.typicode.com/todos";
+            HttpClient client = new HttpClient();
+            var result = await client.GetStringAsync(url);
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
